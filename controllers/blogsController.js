74bd51f2637bb40c3c0227c8c54d53731389617c
emresurmeli <s.emre.s.8@gmail.com>
2024-03-29 16:07:45 -0400

    Create a POST route that allows API to create a new blog post

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -1,5 +1,6 @@
 import fs from 'fs'
 
+// GET
 const getBlogs = async (req, res) => {
   try {
     // TODO: make this a util function to not repeat in each controller method
@@ -46,7 +47,40 @@ const getBlog = async (req, res) => {
   }
 }
 
+// POST
+const createBlog = async (req, res) => {
+  try {
+    const newBlog = req.body
+    const blogs = await fs.promises.readFile(
+      process.cwd() + '/data/blogs.json',
+      { encoding: 'utf8' }
+    )
+    const blogsArray = JSON.parse(blogs)
+    const newBlogId = blogsArray[blogsArray.length - 1].id + 1
+
+    newBlog.id = newBlogId
+    blogsArray.push(newBlog)
+
+    await fs.promises.writeFile(
+      process.cwd() + '/data/blogs.json',
+      JSON.stringify(blogsArray, null, 2),
+      { encoding: 'utf8' }
+    )
+
+    const updateBlogData = await fs.promises.readFile(
+      process.cwd() + '/data/blogs.json',
+      { encoding: 'utf8' }
+    ) 
+
+    res.json(JSON.parse(updateBlogData))
+  } catch (error) {
+    console.error(error)
+    res.status(500).json(error)
+  }
+}
+
 export {
   getBlogs,
-  getBlog
+  getBlog,
+  createBlog
 }
--- routers/blogsRouter.js
+++ routers/blogsRouter.js
@@ -1,11 +1,12 @@
 // Import modules
 import { Router } from 'express'
-import { getBlogs, getBlog } from '../controllers/blogsController.js'
+import { getBlogs, getBlog, createBlog } from '../controllers/blogsController.js'
 
 const router = Router()
 
 // GET routes
 router.get('/', getBlogs)
 router.get('/:id', getBlog)
+router.post('/', createBlog)
 
 export default router
