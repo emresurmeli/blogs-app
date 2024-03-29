f82e836e89c60cdaa6a8e0bccfd4b0d89d25a98b
emresurmeli <s.emre.s.8@gmail.com>
2024-03-29 16:42:59 -0400

    Add PUT route to handle updating a single blog post

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -24,14 +24,14 @@ const getBlogs = async (req, res) => {
 
 const getBlog = async (req, res) => {
   try {
+    const id = req.params.id
     const blogs = await fs.promises.readFile(
       process.cwd() + '/data/blogs.json',
       { encoding: 'utf8' }
     )
     const blogsArray = JSON.parse(blogs)
-    const id = req.params.id
-    const blogPost = blogsArray.find((el) => {
-      return el.id.toString() === id
+    const blogPost = blogsArray.find((blog) => {
+      return blog.id.toString() === id
     })
 
     if(!blogPost) {
@@ -79,8 +79,64 @@ const createBlog = async (req, res) => {
   }
 }
 
+// PUT
+const updateBlog = async (req, res) => {
+  try {
+    let id = req.params.id.toString()
+
+    const blogs = await fs.promises.readFile(
+      process.cwd() + '/data/blogs.json',
+      { encoding: 'utf8' }
+    )
+    const blogsArray = JSON.parse(blogs)
+    let blogToUpdate = blogsArray.find((blog) => {
+      return blog.id === Number(id)
+    })
+
+    if (!blogToUpdate) {
+      res.status(404).json({
+        error: `Could not find blog post with the id of ${id}`
+      })
+    }
+
+    if(!req.body.title || !req.body.body) {
+      res.status(400).json({
+        error: `Please provide a title and a body to update a blog`
+      })
+    }
+
+    blogToUpdate = req.body
+    blogToUpdate.id = Number(id)
+    let indexToUpdate
+    blogsArray.forEach((blog, index) => {
+      if(blog.id === Number(id)) {
+        indexToUpdate = index
+      }
+    })
+
+    blogsArray.splice(indexToUpdate, 1, blogToUpdate)
+    
+    await fs.promises.writeFile(
+      process.cwd() + '/data/blogs.json',
+      JSON.stringify(blogsArray, null, 2),
+      { encoding: 'utf8' }
+    )
+
+    const updatedBlogsArray = await fs.promises.readFile(
+      process.cwd() + '/data/blogs.json',
+      { encoding: 'utf8' }
+    ) 
+
+    res.json(JSON.parse(updatedBlogsArray))
+  } catch (error) {
+    console.error(error)
+    res.status(500).json(error)
+  }
+}
+
 export {
   getBlogs,
   getBlog,
-  createBlog
+  createBlog,
+  updateBlog
 }
--- routers/blogsRouter.js
+++ routers/blogsRouter.js
@@ -1,12 +1,17 @@
 // Import modules
 import { Router } from 'express'
-import { getBlogs, getBlog, createBlog } from '../controllers/blogsController.js'
+import { getBlogs, getBlog, createBlog, updateBlog } from '../controllers/blogsController.js'
 
 const router = Router()
 
 // GET routes
 router.get('/', getBlogs)
 router.get('/:id', getBlog)
+
+// POST routes
 router.post('/', createBlog)
+
+// PUT routes
+router.put('/:id', updateBlog)
 
 export default router
